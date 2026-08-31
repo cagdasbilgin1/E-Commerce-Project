f3903f163c7abb918d27a8cff88d0d6aa00c840a
cagdas bilgin <>
2026-08-31 20:14:35 +0300

    fix(store): correct redux-logger import

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
-import logger from 'redux-logger';
+import { logger } from 'redux-logger';
 import clientReducer from './reducers/clientReducer';
 import productReducer from './reducers/productReducer';
 import shoppingCartReducer from './reducers/shoppingCartReducer';
